5e845123202fe8c3e36e80004a22f712121a46c2
Aiaz Shamasov <aiazshamasov@Aiazs-MacBook-Pro.local>
2019-11-10 12:04:58 -0800

    Loop i
    idid

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+
     }
 }
