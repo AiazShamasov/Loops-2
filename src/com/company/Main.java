54ed5ccc7bc9bad43936f5e70b8c4d94cc534758
Aiaz Shamasov <aiazshamasov@Aiazs-MacBook-Pro.local>
2019-11-10 12:28:34 -0800

    Homework 1
    Complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,32 @@
 package com.company;
 
 import java.sql.SQLOutput;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 1; i < 11; i++) {
+            if(i==7){
+                continue;
+            }
             System.out.println(i);
         }
 
+        int[] array = {1,2,3,4,5};
+        System.out.println(Arrays.toString(array));
+
+        try {
+            int[] numbers = {1, 2, 3, 4, 5,};
+            System.out.println(numbers[6]);
+        }
+        catch(Exception e) {
+            System.out.println("error message");
+        }
     }
+
+
+
+
+
 }
