44d35b42486a01c50ffff83b7290f44fe90f0033
Aiaz Shamasov <aiazshamasov@Aiazs-MacBook-Pro.local>
2019-11-10 18:56:43 -0800

    Homework 2
    Loops and strings

---
+++ src/com/company/Homework2.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+import org.w3c.dom.ls.LSOutput;
+import java.util.Arrays;
+
+public class Homework2 {
+
+    public static void main(String[] args) {
+        String c = "String";
+
+        if (c.length() % 2 == 0)
+            System.out.println("String is even");
+        else
+            System.out.println("String is not even");
+    }
+}
